70f6d143ccbca519e9cf438bdd6064468184b3f1
Philechenim <88583303+Philechenim@users.noreply.github.com>
2021-08-27 07:33:13 -0700

    New python Notebook added

---
+++ learning tricks.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import numpy as np
+a = np.arange(5)
+
+print(a <= 3)
+
+
+# In[1]:
+
+
+a = np.arange(5)
+a + 20
+
+
+# In[4]:
+
+
+import pandas as pd
+
+certificates_earned = pd.Series(
+    [8, 2, 5, 6],
+    index=['Tom', 'Kris', 'Ahmad', 'Beau']
+)
+
+print(certificates_earned)
+
+
+# In[5]:
+
+
+import pandas as pd
+
+certificates_earned = pd.Series(
+    [8, 2, 5, 6],
+    index=['Tom', 'Kris', 'Ahmad', 'Beau']
+)
+
+print(certificates_earned[certificates_earned > 5])
+
+
+# In[6]:
+
+
+import pandas as pd
+
+certificates_earned = pd.DataFrame({
+    'Certificates': [8, 2, 5, 6],
+    'Time (in months)': [16, 5, 9, 12]
+})
+
+certificates_earned.index = ['Tom', 'Kris', 'Ahmad', 'Beau']
+
+print(certificates_earned.iloc[2])
+
+
+# In[7]:
+
+
+import pandas as pd
+
+certificates_earned = pd.DataFrame({
+    'Certificates': [8, 2, 5, 6],
+    'Time (in months)': [16, 5, 9, 12]
+})
+names = ['Tom', 'Kris', 'Ahmad', 'Beau']
+
+certificates_earned.index = names
+longest_streak = pd.Series([13, 11, 9, 7], index=names)
+certificates_earned['Longest streak'] = longest_streak
+
+print(certificates_earned)
+
+
+# In[8]:
+
+
+import pandas as pd
+import numpy as np
+
+s = pd.Series(['a', 3, np.nan, 1, np.nan])
+
+print(s.notnull().sum())
+
